41f56e82c759f3ca9003b8aa088ad20f0f7e0ff6
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2024-11-17 14:51:52 +0330

    dd log

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -117,6 +117,7 @@ class UserController extends Controller
             ], 403);
         }
 
+        dd( $account);
         $db = new Database($account->db_name, $account->db_user, $account->db_pass);
         $db->connect();
         $db->getTables();
