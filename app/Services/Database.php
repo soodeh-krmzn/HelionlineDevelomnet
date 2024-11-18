6f88f5b773b49aae6090d590fd2ad6e25d80951d
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2024-11-18 09:32:29 +0330

    t

--- app/Services/Database.php
+++ app/Services/Database.php
@@ -26,10 +26,11 @@ class Database
     {        
         $key = base64_decode(Config::get('app.custom_key'));
         $encrypter = new Encrypter($key, Config::get('app.cipher'));
+        abort(500, $this->db_user );
         $name = $encrypter->decryptString($this->db_name);
         $user = $encrypter->decryptString($this->db_user);
         $pass = $encrypter->decryptString($this->db_pass);
-        abort(500, $name );
+
         return compact('name', 'user', 'pass');
     }
 
