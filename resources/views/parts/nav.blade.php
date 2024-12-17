320ed4e489196b82859fcae170b1bde01bcbc4bf
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2024-12-17 09:57:22 +0330

    Update nav.blade.php

--- resources/views/parts/nav.blade.php
+++ resources/views/parts/nav.blade.php
@@ -16,6 +16,17 @@
             </a>
         </div>
         <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+            <label class="switch switch-success switch-lg">
+                <input type="checkbox" class="switch-input status-offline">
+                <span class="switch-toggle-slider">
+                    <span class="switch-on">
+                        <i class="bx bx-check"></i>
+                    </span>
+                    <span class="switch-off">
+                        <i class="bx bx-x"></i>
+                    </span>
+                </span>
+            </label>
             <ul class="navbar-nav flex-row align-items-center ms-auto">
                 <li class="nav-item  me-2">
                     <a href="{{ route('chat') }}">
@@ -35,19 +46,6 @@
                             {{ (new \App\Models\Setting())->getDaysLeft() }}
                         </span>
                     </a>
-                </li>
-                <li class="nav-item me-2">
-                    <label class="switch switch-success switch-lg">
-                        <input type="checkbox" class="switch-input status-sms">
-                        <span class="switch-toggle-slider">
-                            <span class="switch-on">
-                                <i class="bx bx-check"></i>
-                            </span>
-                            <span class="switch-off">
-                                <i class="bx bx-x"></i>
-                            </span>
-                        </span>
-                    </label>
                 </li>
                 @if ((new \App\Models\Setting())->getSetting('sms_panel') == false)
                     <li class="nav-item me-2">
